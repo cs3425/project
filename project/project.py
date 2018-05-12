af0df8c0f069c2350900e732fcd1de015d66e587
Cecilia Sena <cs3425@columbia.edu>
2018-05-12 01:11:39 -0400

    updated py file formatting

--- project/project.py
+++ project/project.py
@@ -1,38 +1,52 @@
 #!/usr/bin/env python
 
+import pandas as pd
+
+from bokeh.io import curdoc
+from bokeh.layouts import row, column
+from bokeh.models import ColumnDataSource
+from bokeh.models.widgets import Select
+from bokeh.plotting import figure
+
+
 class Project:
 
-baseurl = "https://www.nodc.noaa.gov/archive/arc0023/0052765/1.1/data/0-data/NODC_2004-2006_Data_Archive/"
-nut_2004_url = "Tijuana%20River/nutrient/data/tjrnut2004.txt"
-nut_2005_url = "Tijuana%20River/nutrient/data/tjrnut2005.txt"
-met_2004_url = "Tijuana%20River/meteorological/data/Tidal%20Linkage/tjrtlmet2004.txt"
-met_2005_url = "Tijuana%20River/meteorological/data/Tidal%20Linkage/tjrtlmet2005.txt"
-DEFAULT_TICKERS = ['CHLA_N', 'NH4F', 'TotPAR', 'ATemp', 'MaxTemp']
+    def __init__(self, **kwargs):
+        self.baseurl = "https://www.nodc.noaa.gov/archive/arc0023/0052765/1.1/data/0-data/NODC_2004-2006_Data_Archive/"
 
-def nix(val, lst):
-    return [x for x in lst if x != val]
+        # allow users to enter or modify other params using kwargs
+        self.params.update(kwargs)
 
-def load_ticker(ticker):
-    nut2004data = pd.read_table(baseurl+nut_2004_url,sep='\t',header=(0),parse_dates=['SMPLDATE'])
-    nut2005data = pd.read_table(baseurl+nut_2005_url,sep='\t',header=(0),parse_dates=['SMPLDATE'])
-    met2004data = pd.read_table(baseurl+met_2004_url,sep='\t',header=(0),parse_dates=['SMPLDATE'])
-    met2005data = pd.read_table(baseurl+met_2004_url,sep='\t',header=(0),parse_dates=['SMPLDATE'])
-    nut2004data = nut2004data.set_index(nut2004df.CHLA_N)
-    nut2005data = nut2005data.set_index(nut2005df.CHLA_N)
-    #met2004data = met2004data.set_index('SMPLDATE')
-    #met2005data = met2005data.set_index('SMPLDATE')
-    return pd.DataFrame({ticker: nut2004df.CHLA_N, ticker+'_returns': nut2004df.CHLA_N.diff()})
+        self.nut_2004_url = "Tijuana%20River/nutrient/data/tjrnut2004.txt"
+        self.nut_2005_url = "Tijuana%20River/nutrient/data/tjrnut2005.txt"
+        self.met_2004_url = "Tijuana%20River/meteorological/data/Tidal%20Linkage/tjrtlmet2004.txt"
+        self.met_2005_url = "Tijuana%20River/meteorological/data/Tidal%20Linkage/tjrtlmet2005.txt"
+        self.DEFAULT_TICKERS = ['CHLA_N', 'NH4F', 'TotPAR', 'ATemp', 'MaxTemp']
+        self.nut2004df = pd.read_table(self.baseurl+self.nut_2004_url,sep='\t',header=(0))
+        self.nut2005df = pd.read_table(self.baseurl+self.nut_2005_url,sep='\t',header=(0))
+        self.met2004df = pd.read_table(self.baseurl+self.met_2004_url,sep='\t',header=(0))
+        self.met2005df = pd.read_table(self.baseurl+self.met_2005_url,sep='\t',header=(0))
 
-def get_data(y1, y2):
-    df1 = load_ticker(y1)
-    df2 = load_ticker(y2)
-    data = pd.concat([df1, df2], axis=1)
-    data = data.dropna()
-    data['y1'] = data[y1]
-    data['y2'] = data[y2]
-    #data['t1_returns'] = data[nut2004df.CHLA_N]
-    #data['t2_returns'] = data[nut2005df.CHLA_N]
-    return data
+    def nix(val, lst):
+        return [x for x in lst if x != val]
+
+    def load_ticker(self, ticker):
+        nut2004data = pd.read_table(self.baseurl+self.nut_2004_url,sep='\t', header=(0), parse_dates=['SMPLDATE'])
+        nut2005data = pd.read_table(self.baseurl+self.nut_2005_url,sep='\t', header=(0), parse_dates=['SMPLDATE'])
+        # met2004data = pd.read_table(self.baseurl+met_2004_url,sep='\t', header=(0), parse_dates=['SMPLDATE'])
+        # met2005data = pd.read_table(self.baseurl+met_2004_url,sep='\t', header=(0), parse_dates=['SMPLDATE'])
+        nut2004data = nut2004data.set_index(self.nut2004df.CHLA_N)
+        nut2005data = nut2005data.set_index(self.nut2005df.CHLA_N)
+        return pd.DataFrame({ticker: self.nut2004df.CHLA_N, ticker+'_returns': self.nut2004df.CHLA_N.diff()})
+
+    def get_data(y1, y2):
+        df1 = load_ticker(y1)
+        df2 = load_ticker(y2)
+        data = pd.concat([df1, df2], axis=1)
+        data = data.dropna()
+        data['y1'] = data[y1]
+        data['y2'] = data[y2]
+        return data
 
 # set up widgets
 
@@ -62,13 +76,16 @@ ys2.circle('date', 'y2', size=1, source=source, color=None, selection_color="ora
 
 # set up callbacks
 
+
 def ticker1_change(attrname, old, new):
     ticker2.options = nix(new, DEFAULT_TICKERS)
     update()
 
+
 def ticker2_change(attrname, old, new):
     ticker1.options = nix(new, DEFAULT_TICKERS)
     update()
+
 
 def update(selected=None):
     y1, y2 = ticker1.value, ticker2.value
@@ -80,8 +97,10 @@ def update(selected=None):
     corr.title.text = '%s returns vs. %s returns' % (y1, y2)
     ys1.title.text, ys2.title.text = y1, y2
 
+
 ticker1.on_change('value', ticker1_change)
 ticker2.on_change('value', ticker2_change)
+
 
 def selection_change(attrname, old, new):
     y1, y2 = ticker1.value, ticker2.value
@@ -89,6 +108,7 @@ def selection_change(attrname, old, new):
     selected = source.selected.indices
     if selected:
         data = data.iloc[selected, :]
+
 
 source.on_change('selected', selection_change)
 
@@ -102,4 +122,4 @@ layout = column(main_row, series)
 update()
 
 curdoc().add_root(layout)
-curdoc().title = "Stocks"
+curdoc().title = "Nutrients"
